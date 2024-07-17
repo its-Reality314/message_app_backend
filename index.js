47c3b0eafd9946ae52911ef1a0aa7162af287590
Aman chandra <124434129+its-Reality314@users.noreply.github.com>
2024-07-17 20:08:56 +0530

    Update index.js 1

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:3000',
+    origin:'https://unique-pudding-d1b9c9.netlify.app/',
     credentials:true
 };
 app.use(cors(corsOption)); 
