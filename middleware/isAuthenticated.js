63da3a0f7f2de02c50605aa4c6a41a2c392ea12a
Aman chandra <124434129+its-Reality314@users.noreply.github.com>
2024-07-18 02:02:31 +0530

    Update isAuthenticated.js

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,16 +1,31 @@
 import jwt from "jsonwebtoken";
+
+import { User } from "../models/userModel.js";
+
 const isAuthenticated = async(req,res,next) => {
   try {
-    const token = req.cookies.token;
+    // const token = req.cookies.token;
+    // let token = req.body;
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    // console.log("token........",token);
+    // token=JSON.stringify(token);
+    // console.log("token........",token);
+    // token=JSON.parse(token);
+    console.log("token........",token);
     if(!token){
         return res.status(401).json({message:"User not authenticated."})
     };
     const decode = await jwt.verify(token,process.env.JWT_SECRET_KEY);
+    // const decode = await User.find({username:token.userName});
+    console.log(decode);
+
     if(!decode){
         return res.status(401).json({message:"Invalid token"});
         
     };
     req.id = decode.userId;
+    console.log("sending........")
     next();
   } catch (error) {
     console.log(error);
