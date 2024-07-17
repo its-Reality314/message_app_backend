e7ef327c950231092da17da3d268c53323ec4996
Aman chandra <124434129+its-Reality314@users.noreply.github.com>
2024-07-17 22:52:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,19 +11,37 @@ dotenv.config({});
 
  
 const PORT = process.env.PORT || 5000;
-
-// middleware
-app.use(express.urlencoded({extended:true}));
-app.use(express.json()); 
-app.use(cookieParser());
-
-//
 const corsOption={
     origin:'https://statuesque-sunburst-a1b092.netlify.app',
     credentials:true
 };
 app.use(cors(corsOption)); 
 app.set("trust proxy",1);
+
+app.use(session({
+    name: 'session',
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        //secure: true,
+        //expires: Date.now() + 1000 * 60 * 60 * 24,
+        maxAge: 1000 * 60 * 15 
+    },
+    store: MongoStore.create({
+      mongoUrl: dbUrl,
+      secret:'secret',
+      touchAfter: 24 * 60 * 60
+    })
+}));
+// middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.json()); 
+app.use(cookieParser());
+
+//
+
 
 
 // routes
