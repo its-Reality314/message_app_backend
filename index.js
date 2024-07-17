fc60030b3438a7824346832369c2eebb0b22053c
Aman chandra <124434129+its-Reality314@users.noreply.github.com>
2024-07-17 22:54:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,23 +18,7 @@ const corsOption={
 app.use(cors(corsOption)); 
 app.set("trust proxy",1);
 
-app.use(session({
-    name: 'session',
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        //secure: true,
-        //expires: Date.now() + 1000 * 60 * 60 * 24,
-        maxAge: 1000 * 60 * 15 
-    },
-    store: MongoStore.create({
-      mongoUrl: dbUrl,
-      secret:'secret',
-      touchAfter: 24 * 60 * 60
-    })
-}));
+
 // middleware
 app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
